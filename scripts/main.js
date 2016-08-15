1eb26f572b5f0c0d15594e43a3c31d961bee1588
JONA FRANK <jona.mailbox@gmail.com>
2016-08-15 23:41:04 +0530

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,8 @@
+window.onload = function(){
+	var space = document.getElementById('space');
+    var parallax = new Parallax(space);
+}
 
+function planet_details(planet){
+	console.log(planet+' clicked');
+}
